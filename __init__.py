335615c6fa42dc1df09dc14d80c9e08dfad856fd
Gaurav Goyal <gaurav.goyal@openlabs.co.in>
2014-07-07 16:37:19 +0530

    Add and view Payment profiles of current user #4620
    Review Id: 9873001

--- __init__.py
+++ __init__.py
@@ -6,9 +6,12 @@
     :license: BSD, see LICENSE for more details.
 """
 from trytond.pool import Pool
+from party import Party, PaymentProfile
 
 
 def register():
     Pool.register(
+        Party,
+        PaymentProfile,
         module='nereid_payment_gateway', type_='model'
     )
