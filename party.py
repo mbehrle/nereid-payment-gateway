2e62d61c897349a6483ba2a4f7423fd43ccb4b68
Prakash Pandey <prakash.pandey@fulfil.io>
2015-08-11 15:39:06 +0530

    Module is maintained by Fulfil.IO

--- party.py
+++ party.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-    party.py
-
-    :copyright: (c) 2014-2015 by Openlabs Technologies & Consulting (P) Limited
-    :license: BSD, see LICENSE for more details.
-"""
 from datetime import datetime
 
 from trytond.pool import PoolMeta, Pool
